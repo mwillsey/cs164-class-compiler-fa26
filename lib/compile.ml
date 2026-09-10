63283dc04b5ad9e7816d9a4b9a99b2a16d5c30f0
Max Willsey <me@mwillsey.com>
2026-09-10 09:30:32 -0700

    finish lec 3

--- lib/compile.ml
+++ lib/compile.ml
@@ -1,18 +1,24 @@
-(* open S_exp *)
-(* open Shared.Directive *)
+open S_exp
+open Shared.Directive
 
-let compile (program: string) : string =
-  String.concat "\n" [
-    "global _entry";
-    "_entry:";
-    Printf.sprintf "    mov rax, %s" program;
-    "    ret"
-  ]
+let rec compile_exp (exp: s_exp): directive list =
+  match exp with
+  Num n -> [Mov (Reg Rax, Imm n)]
+  | Lst [Sym "add1"; l] -> 
+    compile_exp l @ [ Add (Reg Rax, Imm 1) ]
+  | _ -> failwith "I can't handle that sexp"
+
+let compile (program: s_exp) : string =
+  let directives = compile_exp program in
+    [ Global "entry"; Label "entry"; ] @ directives @ [Ret ]
+    |> List.map (string_of_directive ~macos:true)
+    |> String.concat "\n"
 
 
 let compile_to_file (program: string): unit = 
   let file = open_out "program.s" in
-  output_string file (compile program);
+  let s_exp = parse program in
+  output_string file (compile s_exp);
   close_out file
 
 let compile_and_run (program: string): string =
